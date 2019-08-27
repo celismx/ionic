ff40e4ceb701db2921e4b76be85c18cf865e5ec1
Sebastian Alonso Gomez Arias <seagomezar@gmail.com>
2019-08-27 12:28:13 -0500

    implementando logout y otras opciones del menu clase 19

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -56,7 +56,7 @@ export class LoginPage implements OnInit {
       .then(res => {
         this.errorMessage = "";
         this.storage.set("isUserLoggedIn", true);
-        this.navCtrl.navigateForward("/home");
+        this.navCtrl.navigateForward("/menu/home");
       })
       .catch(err => {
         this.errorMessage = err;
--- src/app/menu/menu.page.ts
+++ src/app/menu/menu.page.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { MenuController } from "@ionic/angular";
+import { NavController } from "@ionic/angular";
+import { Storage } from "@ionic/storage";
 
 @Component({
-  selector: 'app-menu',
-  templateUrl: './menu.page.html',
-  styleUrls: ['./menu.page.scss'],
+  selector: "app-menu",
+  templateUrl: "./menu.page.html",
+  styleUrls: ["./menu.page.scss"]
 })
 export class MenuPage implements OnInit {
+  constructor(
+    private menu: MenuController,
+    private navCtrl: NavController,
+    private storage: Storage
+  ) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  closeMenu() {
+    this.menu.close();
   }
-
+  logout() {
+    this.storage.remove("isUserLoggedIn");
+    this.navCtrl.navigateRoot("/login");
+  }
 }
