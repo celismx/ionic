f2fa691d4d1d1fe59858178ca2ab71cdc363a59d
Sebastian Alonso Gomez Arias <seagomezar@gmail.com>
2019-08-28 09:03:37 -0500

    pintando artistas y albums en el home

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { PlatziMusicService } from "../services/platzi-music.service";
 
 @Component({
   selector: "app-home",
@@ -13,5 +14,19 @@ export class HomePage {
     centeredSlides: true,
     speed: 400
   };
-  constructor() {}
+  songs: any[] = [];
+  albums: any[] = [];
+  constructor(private musicService: PlatziMusicService) {}
+
+  ionViewDidEnter() {
+    this.musicService.getNewReleases().then(newReleases => {
+      this.artists = newReleases.albums.items;
+      this.songs = newReleases.albums.items.filter(
+        e => e.album_type == "single"
+      );
+      this.albums = newReleases.albums.items.filter(
+        e => e.album_type == "album"
+      );
+    });
+  }
 }
